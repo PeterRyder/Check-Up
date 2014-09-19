055d713d16be521a9a3e870807b0eabd53e49f64
PeterRyder <peter.w.ryder@gmail.com>
2014-09-19 14:39:15 -0400

    Added useful comments to MainWindow functions

--- Check-Up/Check-Up/MainWindow.cs
+++ Check-Up/Check-Up/MainWindow.cs
@@ -14,12 +14,15 @@ namespace Check_Up {
     public partial class MainWindow : Form {
         DataCollection dataCollector;
         int cycles = 1;
+
         public MainWindow() {
             InitializeComponent();
+
+            // initialize a data collector
             dataCollector = new DataCollection();
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
+        private void MainWindow_Load(object sender, EventArgs e) {
 
         }
 
@@ -33,16 +36,22 @@ namespace Check_Up {
             this.cycles = 1;
             button_monitorStop.Enabled = true;
 
+            // Load settings
             dataCollector.ReadSettings();
 
+            // Begin the backgroundWorker
             backgroundWorker1.RunWorkerAsync();
 
+            // Add the ProgressChanged function to the ProgressChangedEventHandler
             backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker1_ProgressChanged);
 
+            // check if data should be gathered
             if (dataCollector.shouldGatherData) {
                 backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
             }
             else {
+
+                // Stop the backgroundWorker if data shouldn't be gathered
                 backgroundWorker1.CancelAsync();
                 backgroundWorker1.ReportProgress(100);
             }
@@ -85,20 +94,29 @@ namespace Check_Up {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+
+            // Update the progress bar every time the backgroundWorker changes
             progressBar1.Value = e.ProgressPercentage;
+
+            // Update the label next to the progress bar
             label_percentage.Text = e.ProgressPercentage.ToString() + "%";
+
+            // If CPU data should be gathered, update the graph
             if (Properties.Settings.Default.CPU) {
                 updateGraph("CPU", "" + cycles, "" + dataCollector.currentCPUUsage);
             }
 
+            // If Memory data should be gathered, update the graph
             if (Properties.Settings.Default.Memory) {
                 updateGraph("Memory", "" + cycles, "" + dataCollector.currentMemUsage);
             }
 
+            // If Network data should be gathered, update the graph
             if (Properties.Settings.Default.Network && dataCollector.canGatherNet) {
                 updateGraph("Network", "" + cycles, "" + dataCollector.currentNetUsageMBs);
             }
 
+            // If Disk IO data should be gathered, update the graph
             if (Properties.Settings.Default.DiskIO) {
                 updateGraph("Disk", "" + cycles, "" + dataCollector.percentDiskTime);
             }
@@ -106,12 +124,15 @@ namespace Check_Up {
         }
 
         /// <summary>
-        /// DoWork method for the background worker - gathers data
+        /// DoWork method for the backgroundWorker - gathers data
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
+            // Create a BackgroundWorker object out of the sender
             BackgroundWorker worker = sender as BackgroundWorker;
+
+            // GatherData every time the background worker does work
             e.Result = GatherData(worker, e);
         }
 
@@ -129,7 +150,11 @@ namespace Check_Up {
             if (e.Error != null) {
                 MessageBox.Show(e.Error.Message);
             }
+
+            // Enable the gatherData button when the backgroundWorker is completed
             button_gatherData.Enabled = true;
+
+            // Disable the monitorStop button when the backgroundWorker is completed
             button_monitorStop.Enabled = false;
         }
 
@@ -140,7 +165,7 @@ namespace Check_Up {
         /// <param name="e"></param>
         /// <returns></returns>
         private bool GatherData(BackgroundWorker sender, DoWorkEventArgs e) {
-
+            // Store the pollingTime and pollingInterval settings
             double pollingTime = Properties.Settings.Default.PollingTime;
             double pollingInterval = Properties.Settings.Default.PollingInterval;
 #if DEBUG
@@ -148,6 +173,8 @@ namespace Check_Up {
             Console.WriteLine("Polling Interval: " + pollingInterval);
 #endif
             for (double i = pollingInterval; i <= pollingTime; ) {
+
+                // If there is a pending cancellation break out of the loop
                 if (backgroundWorker1.CancellationPending) {
 #if DEBUG
                     Console.WriteLine("Cancellation is pending - killing the loop");
@@ -156,10 +183,13 @@ namespace Check_Up {
                     return true;
                 }
                 
+                // Gather data on the devices
                 dataCollector.GatherData();
 
+                // If the pollingTime is to be used
                 if (!Properties.Settings.Default.IgnoreTime) {
 
+                    // Calculate the percentage of the work which has been done
                     double percentage = (i / pollingTime) * 100;
 #if DEBUG
                     Console.WriteLine("Percentage: " + percentage);
@@ -173,24 +203,34 @@ namespace Check_Up {
 #endif
                     }
                     else {
+                        // Report the progress percentage
                         percentage = Math.Round(percentage);
                         backgroundWorker1.ReportProgress((int)percentage);
 #if DEBUG
                         Console.WriteLine("I {0}", i);
 #endif
                     }
+
+                    // Increment the amount of time elapsed
                     i += pollingInterval;
                 }
                 else {
+
+                    // If the polling time is not to be used keep the progress bar at 0 percent
                     backgroundWorker1.ReportProgress(0);
                 }
 
+                // Sleep the backgroundWorker for the pollingInterval
                 Thread.Sleep((int)(pollingInterval * 1000));
+
+                // Increment the amount of cycles which has elapsed
                 cycles++;
             }
 
+            // If the loop is finished stop the backgroundWorker
             backgroundWorker1.CancelAsync();
 
+            // Announce if there is a pending cancellation
             if (backgroundWorker1.CancellationPending) {
 #if DEBUG
                 Console.WriteLine("Cancellation is pending");
@@ -213,36 +253,34 @@ namespace Check_Up {
             // Could be useful when creating a doughnut type graph
             //resetChartFunc();
 
+            // Check if the data type is already registered with the chart
             if (this.chart.Series.IndexOf(type) != -1) {
 #if DEBUG
                 //Console.WriteLine("Chart already has {0} in the series", type);
 #endif
             }
             else {
+                // If the data type is not registered with the chart add it
                 this.chart.Series.Add(type);
             }
 
             try {
+                // Create a line graph out of the data
                 this.chart.Series[type].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             }
             catch {
                 Console.WriteLine("Couldn't change {0} into line graph", type);
             }
             try {
+                // Add the X,Y coordinate of the data to the graph
                 this.chart.Series[type].Points.AddXY(x, y);
+
+                // Display a tooltip when mouse hovers over the line graph
                 chart.Series[type].ToolTip = "X: #VALX, Y: #VALY";
             }
             catch {
                 Console.WriteLine("Couldn't create point on graph X: {0}, Y: {1}", x, y);
             }
-        }
-
-        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e) {
-
-        }
-
-        private void MainWindow_Load(object sender, EventArgs e) {
-
         }
 
         /// <summary>
@@ -251,8 +289,14 @@ namespace Check_Up {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void monitorStop_Click(object sender, EventArgs e) {
+
+            // When the monitorStop button is clicked stop the backgroundWorker
             backgroundWorker1.CancelAsync();
+
+            // Announce that the backgroundWorker is 100 percent done
             backgroundWorker1.ReportProgress(100);
+
+            // Enable the gatherData button
             button_gatherData.Enabled = true;
         }
 
