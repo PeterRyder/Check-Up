d0195c6f465b9fd32327e4febe98c2d91d67ea29
PeterRyder1 <peter.w.ryder@gmail.com>
2014-10-06 12:41:00 -0400

    Updated example script with comments

--- example scripts/CPU_Monitor.py
+++ example scripts/CPU_Monitor.py
@@ -6,20 +6,31 @@ Created on Aug 12, 2014
 
 import sys
 import time
+
+# import this C# library to monitor the CPU 
 import System.Diagnostics
 
 
 def main():
     print("Running script...")
 
+    # create a PerformanceMonitor object
     perf = System.Diagnostics.PerformanceCounter("Processor Information",
                                                  "% Processor Time",
                                                  "_Total")
+
+    # call NextValue() once to initialize the counter
     perf.NextValue()
 
     while(True):
+
+        # poll the cpu
         cpuUsage = int(perf.NextValue())
+
+        # do something with the data
         print("Python: Cpu Usage " + str(cpuUsage))
+
+        # sleep for at least .05 seconds
         time.sleep(1)
 
 
