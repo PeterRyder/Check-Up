6d0d097bc646ba810bc652fca762be1501391294
PeterRyder <peter.w.ryder@gmail.com>
2015-02-24 23:13:08 -0500

    added test method for travis ci

--- Check Up/CheckUpUnitTests/ScriptControlTests.cs
+++ Check Up/CheckUpUnitTests/ScriptControlTests.cs
@@ -82,5 +82,10 @@ namespace CheckUpUnitTests {
             Assert.AreEqual(expected, returned);
         }
 
+        [Test]
+        public void TestTravisCI() {
+            
+        }
+
     }
 }
