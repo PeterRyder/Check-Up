fec843693b1eb4b180115102d0d407b6dae3bda1
PeterRyder <peter.w.ryder@gmail.com>
2015-03-21 12:11:33 -0400

    Moved Util classes to Util folder and all windows now show correct icon

--- Check Up/Check Up/Util/BackgroundData.cs
+++ Check Up/Check Up/Util/BackgroundData.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 
-namespace Check_Up {
+namespace Check_Up.Util {
     public class BackgroundDataList : ObservableCollection<BackgroundData> {
 
         public BackgroundDataList()
