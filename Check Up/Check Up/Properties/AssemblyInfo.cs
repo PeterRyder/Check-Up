dbb0936c4d451deaef5bfae29babb63627b7f4a2
PeterRyder <peter.w.ryder@gmail.com>
2015-03-22 12:06:19 -0400

    added sqllite dependencies

--- Check Up/Check Up/Properties/AssemblyInfo.cs
+++ Check Up/Check Up/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RCOS")]
 [assembly: AssemblyProduct("Check Up")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
