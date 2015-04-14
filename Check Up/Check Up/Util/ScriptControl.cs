3d9a9450ca08792b63f18cbe2a7779997cafdd68
PeterRyder <peter.w.ryder@gmail.com>
2015-04-14 16:22:58 -0400

    Completely removed IronPython

--- Check Up/Check Up/Util/ScriptControl.cs
+++ Check Up/Check Up/Util/ScriptControl.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using IronPython.Hosting;
-using Microsoft.Scripting.Hosting;
 using System.IO;
 using System.Diagnostics;
 using System.ComponentModel;
